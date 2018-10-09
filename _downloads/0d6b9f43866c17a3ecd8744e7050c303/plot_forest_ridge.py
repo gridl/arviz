ff30646e04bd954c0ddd010faaeaee87adc77146
Travis CI User <travis@example.org>
2018-10-09 18:15:27 +0000

    Update documentation

---
+++ _downloads/0d6b9f43866c17a3ecd8744e7050c303/plot_forest_ridge.py
@@ -0,0 +1,19 @@
+"""
+Ridgeplot
+=========
+
+_thumb: .8, .5
+"""
+import arviz as az
+
+az.style.use('arviz-darkgrid')
+
+non_centered_data = az.load_arviz_data('non_centered_eight')
+fig, axes = az.plot_forest(non_centered_data,
+                           kind='ridgeplot',
+                           var_names=['theta'],
+                           combined=True,
+                           textsize=11,
+                           ridgeplot_overlap=3,
+                           colors='white')
+axes[0].set_title('Estimated theta for eight schools model', fontsize=11)
